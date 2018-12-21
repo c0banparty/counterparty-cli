3daec1cc26b449c42a9fcb7447d7b1095d484310
c0ban-project <39722780+c0ban-project@users.noreply.github.com>
2018-12-21 16:11:05 +0900

    Support for regtest mode

--- create_send.py
+++ create_send.py
@@ -3,27 +3,40 @@
 from datetime import datetime
 import os
 import sys
+from optparse import OptionParser
 import time
 
 from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
 from counterpartylib.lib import util
 from counterpartylib.lib import config
 
-config.RPC = 'http://rpc:rpcpassword@localhost:4000/api/'
+parser = OptionParser()
+parser.add_option("-r", "--regtest", dest="regtest", default=True)
+parser.add_option("-t", "--testnet", dest="testnet", default=False)
+(options, args) = parser.parse_args()
+
+# counterparty-lib
+XCB_RPC_PORT = config.DEFAULT_RPC_PORT
+if options.regtest:
+    XCB_RPC_PORT = config.DEFAULT_RPC_PORT_REGTEST
+elif options.testnet:
+    XCB_RPC_PORT = config.DEFAULT_RPC_PORT_TESTNET
+
+config.RPC = "http://rpc:rpcpassword@localhost:{}/api/".format(XCB_RPC_PORT)
 config.BACKEND_URL = 'http://localhost'
 config.BACKEND_SSL_NO_VERIFY = False
 config.REQUESTS_TIMEOUT = 1
 
-# rpc_user and rpc_password are set in the bitcoin.conf file
-RPC_USER = os.environ['RPC_USER'] if 'RPC_USER' in os.environ else 'c0ban'
-RPC_PASSWORD = os.environ['RPC_PASSWORD'] if 'RPC_PASSWORD' in os.environ else 'c0ban'
-RPC_HOST = os.environ['RPC_HOST'] if 'RPC_HOST' in os.environ else 'party-c0band-lyra2rev2'
-RPC_PORT = os.environ['RPC_PORT'] if 'RPC_PORT' in os.environ else '3882'
 
-connection_str = "http://{}:{}@{}:{}".format(
-    RPC_USER, RPC_PASSWORD, RPC_HOST, RPC_PORT)
-print(connection_str)
-rpc_connection = AuthServiceProxy(connection_str)
+# c0band
+CBN_RPC_USER = os.environ['RPC_USER'] if 'RPC_USER' in os.environ else 'c0ban'
+CBN_RPC_PASSWORD = os.environ['RPC_PASSWORD'] if 'RPC_PASSWORD' in os.environ else 'c0ban'
+CBN_RPC_HOST = os.environ['RPC_HOST'] if 'RPC_HOST' in os.environ else 'party-c0band-lyra2rev2'
+CBN_RPC_PORT = os.environ['RPC_PORT'] if 'RPC_PORT' in os.environ else '23882'
+
+cbn_connection_str = "http://{}:{}@{}:{}".format(
+    CBN_RPC_USER, CBN_RPC_PASSWORD, CBN_RPC_HOST, CBN_RPC_PORT)
+cbn_rpc_connection = AuthServiceProxy(cbn_connection_str)
 
 def counterparty_api(method, params):
     # import pdb; pdb.set_trace()
@@ -31,7 +44,7 @@ def counterparty_api(method, params):
 
 
 def c0ban_api(method, param):
-    c0band = getattr(rpc_connection, method)
+    c0band = getattr(cbn_rpc_connection, method)
     return c0band(param)
 
 def do_create_broadcast(source, fee_fraction, text, timestamp, value):
